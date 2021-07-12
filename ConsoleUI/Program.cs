5792394fbfd5af51b9e2f48719541fed10197979
RyanCortino <RyanCortino@gmail.com>
2021-07-12 08:46:17 -0500

    Setup Logger Configuration
    Added methods to initialize the builder and logger objects.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 // DI, Serilog, Settings
 
@@ -10,7 +11,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Starting.");
         }
 
         static void BuildConfig(IConfigurationBuilder builder)
